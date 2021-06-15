bb6b155c957ef5bc6b42c5e47cfd166d273e86e6
Venus <tmdiasdutra@icloud.com>
2021-06-15 16:15:10 -0300

    pokedex terca-feira

--- pokedex/src/components/CardPokemon/index.js
+++ pokedex/src/components/CardPokemon/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import { Main, ButtonsTag, ImgTag } from "./styled";
 import { useHistory } from "react-router-dom";
 import { goToDetailPage, goToPokedexPage } from "../../routes/cordinator";
+import useRequestData from "../../hooks/useRequestData";
+import { BASE_URL } from "../../constants/baseUrl";
+import axios from "axios";
 
 export default function CardPokemon() {
     const history = useHistory()
+    const pokemons = (useRequestData(BASE_URL, {})).results
+    const teste = (useRequestData(`${BASE_URL}/1`, {}))
+    console.log(pokemons)
 
+    // const listPokemons = pokemons && pokemons.map((pokemon) => {
+    //     const getPokemons = (() => {
+    //         axios.get(pokemon.url)
+    //             .then((res) => {
+    //                 setData(res.data)
+    //             });
+    //     });
+    // }) 
+ 
     return (
         <Main>
             <ImgTag>
--- pokedex/src/hooks/useRequestData.js
+++ pokedex/src/hooks/useRequestData.js
@@ -1,9 +1,37 @@
 import axios from "axios";
-import { BASE_URL } from "../constants/baseUrl";
+import { useEffect, useState } from "react";
 
-const useRequestData = ((url) => {
-    axios.get(`${BASE_URL}/${url}`)
-    .then((res) => {
-        console.log(res)
-    })
-})
+
+const useRequestData = ((url, initialState) => {
+    const [data, setData] = useState(initialState);
+    const getPokemons = (() => {
+        axios.get(url)
+            .then((res) => {
+                setData(res.data)
+            });
+    });
+    useEffect(() => {
+        getPokemons()
+    }, []);
+
+    // const detailsPokemon = getPokemons() && getPokemons.map((pokemon) => {
+    //     axios.get(pokemon.url)
+    //     .then(() => {
+    //         console.log("oi")
+    //     });
+    // });
+    // return (
+    //     <div>
+    //         { () => getPokemons() && getPokemons.map((pokemon) => {
+    //             return(
+    //                 <p>{pokemon.data}</p>
+    //             )
+    //         })}
+    //     </div>
+    // )
+
+console.log(data)
+    return data;
+});
+
+export default useRequestData;
