413ed09f967522a0efecd63ca579348f5c1d03e4
camilyAbade <laceyj2.laceyj2@gmail.com>
2021-06-18 21:20:11 -0300

    mudando a estilização

--- pokedex/src/components/DetailPokemon/StyledDetailPage.js
+++ pokedex/src/components/DetailPokemon/StyledDetailPage.js
@@ -1,84 +1,147 @@
 import styled from "styled-components";
+import backgroundLigth from "../../img/backgroundLigth.png"
 
-
-export const Geral = styled.div`
+export const Container = styled.div`
     width: 100%;
-    height: 80vh;
+    height: auto;
     display: flex;
-    color: #cccccc;
+    flex-direction: column;
+    background-image: url(${backgroundLigth});
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
 `
-export const Tag = styled.div`
+export const Identificacao = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0.4rem;
+`
+
+export const Button = styled.div`
+    margin-bottom: 1rem;
     button{
         border-radius: 10px;
         border: none;
-        margin: 1rem;
-        width: 10vw;
-        background-color: white;
-        &:hover{
-            background-color: #35ACCE;
-        }
+        padding: 0.5rem;
+        background-color: #30a7d7;
+        cursor: pointer;
+
     }
-    width: 20%;
+`
+
+export const Pokemon = styled.div`
+    width: 100%;
+    height: 80vh;
+    display: flex;
+`
+
+export const Img = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        background-color: #f2f2f2;
+        padding: 3rem;
+        border: 1px solid black;
+    }
+    .teste {
+         position: relative;
+     }
+     .teste .seg {
+         visibility: hidden;
+         opacity: 0;
+         position: absolute;
+         top: 0;
+         left: 0;
+     }
+     .teste:hover .seg {
+         visibility: visible;
+         opacity: 1;
+     }
+     .teste:hover .pri {
+         visibility: hidden;
+         opacity: 0;
+     }
+     .pri {
+         height: 200px;
+         width: 200px;
+     }
+     .seg {
+         height: 200px;
+         width: 200px; 
+     }
+
+`
+
+export const About = styled.div`
+    width: 50%;
+    height: 100%;
     display: flex;
     flex-direction: column;
+
+`
+
+export const Type = styled.div`
+    display: flex;
+    justify-content: space-around;
     background-color: #313131;
-    padding-left: 2rem;
-    min-height: 85vh;
-    max-height: 85vh;
+    color: #c1c1c1;
+    div{
+        display: flex;
+        justify-content: center;
+        width: 6vw;
+        border-radius: 60%;
+    }
+`
+
+export const Card1 = styled.div`
+    display: flex;
+    color: #c1c1c1;
+
+
+    div{
+        display: flex;
+        max-height: 60vh;
+
+    }
+
+`
+export const Abilities = styled.div`
+    width: 50%;
+    height: 60vh;
+    flex-direction: column;
+    background-color: #313131;
+    padding: 0 0 0 1rem;
+    margin: 1rem;
+    border-radius: 40px / 20px 25em 30px 35em;
     overflow-y:hidden; 
     overflow:scroll; 
     ::-webkit-scrollbar {
     display: none;
     }
 
+   
 `
 
-export const ContainerType = styled.div`
+export const Stats = styled.div`
+    width: 50%;
+    min-height: 60vh;
+    max-height: 60vh;
     display: flex;
-    p{
-        margin: 0;
-    }
-
-`
-export const ContainerStats = styled.div`
-    display: flex;
-    margin-top: -30px;
-`
-export const ContainerAbilities = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin-top: -30px;
-`
-export const Sprites = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 80%;
-    height: 85vh;
-    .teste {
-        position: relative;
-    }
-    .teste .seg {
-        visibility: hidden;
-        opacity: 0;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-    .teste:hover .seg {
-        visibility: visible;
-        opacity: 1;
-    }
-    .teste:hover .pri {
-        visibility: hidden;
-        opacity: 0;
-    }
-    .pri {
-        height: 500px;
-        width: 500px;
-    }
-    .seg {
-        height: 500px;
-        width: 500px; 
+    flex-direction: column;
+    background-color: #313131;
+    padding: 0 0 0 1rem;
+    margin: 1rem;
+    border-radius: 40px / 20px 25em 30px 35em;
+    overflow-y:hidden; 
+    overflow:scroll; 
+    ::-webkit-scrollbar {
+    display: none;
     }
 `
